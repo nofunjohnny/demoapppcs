846c47a487fa91aa4ab3e879952c2e1bba938f9b
John Zapata <theorganizedkhaos@icloud.com>
2019-02-16 09:46:00 -0600

    commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'typeface-roboto';
 
 export default class App extends React.Component {
   render() {
@@ -17,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
   },
 });
